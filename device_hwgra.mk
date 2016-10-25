b24153bb5d77315269357e85a3302932a9cb6c64
nexolight <snow.dream.ch@gmail.com>
2016-10-25 22:38:09 +0200

    Fixed overlay
    Change-Id: Icd97081818bd29453e62763e9c63c0f17914e65e

--- device_hwgra.mk
+++ device_hwgra.mk
@@ -6,7 +6,7 @@ $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
 $(call inherit-product-if-exists, vendor/huawei/hwgra/hwgra-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += \
+PRODUCT_PACKAGE_OVERLAYS += \
 	device/huawei/hwgra/overlay
 
 
