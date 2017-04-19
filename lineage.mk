403e658ced918ca5917179ed99418fda7bde2814
Andrea Dieni <andrea.dieni@gmail.com>
2017-04-19 15:05:02 +0200

    change inhert order and addo some properties
    Change-Id: I5af1d4a38a97be6fd06c836edfb6b77532385aae

--- lineage.mk
+++ lineage.mk
@@ -1,15 +1,24 @@
 # Release name
 PRODUCT_RELEASE_NAME := GRA-L09
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/huawei/hwgra/full_hwgra.mk)
 
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+BOARD_VENDOR := huawei
+
+PRODUCT_GMS_CLIENTID_BASE := android-huawei
+
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE:=hwgra
-PRODUCT_NAME:=lineage_hwgra
-PRODUCT_BRAND:=HUAWEI
-PRODUCT_MODEL:=HUAWEI GRA-L09
-PRODUCT_MANUFACTURER:=HUAWEI
+PRODUCT_DEVICE := hwgra
+PRODUCT_NAME := lineage_hwgra
+PRODUCT_BRAND := HUAWEI
+PRODUCT_MODEL := HUAWEI GRA-L09
+PRODUCT_MANUFACTURER := HUAWEI
