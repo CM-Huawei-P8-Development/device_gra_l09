bd6a54dddc339e1ef8231c7a22eef05ccd07b733
Andrea Dieni <andrea.dieni@gmail.com>
2017-05-04 21:10:38 +0200

    Power HAL reworked
    Modified to use interactive and ondemand governors on the two cpusets, dt2w from Meticulus one
    
    Change-Id: Id929ea05257b2397d7cda75ab28ff534d9f80705

--- power/power.h
+++ power/power.h
@@ -19,7 +19,9 @@
 #define STATE_ON "state=1"
 
 #define KERNEL_HMP_PATH "/sys/kernel/hmp/"
+#define DDRFREQ__PATH	"/sys/class/devfreq/ddrfreq"
 #define GPUFREQ_PATH "/sys/class/devfreq/gpufreq/"
+#define GPU_ONDEMAND_PATH	"/sys/class/devfreq/gpufreq/mali_ondemand"
 
 #define CPUFREQ_PATH0 "/sys/devices/system/cpu/cpu0/cpufreq/"
 #define CPUFREQ_PATH1 "/sys/devices/system/cpu/cpu4/cpufreq/"
@@ -27,8 +29,11 @@
 #define INTERACTIVE_PATH0 "/sys/devices/system/cpu/cpu0/cpufreq/interactive/"
 #define INTERACTIVE_PATH1 "/sys/devices/system/cpu/cpu4/cpufreq/interactive/"
 
+#define ONDEMAND_PATH0 "/sys/devices/system/cpu/cpu0/cpufreq/ondemand/"
+#define ONDEMAND_PATH1 "/sys/devices/system/cpu/cpu4/cpufreq/ondemand/"
+
 #define TAP_TO_WAKE_NODE "/sys/touchscreen/easy_wakeup_gesture"
-#define TAP_TO_WAKE_UP "/sys/touchscreen/wakeup_gesture_enable"
+#define TAP_TO_WAKE_ENABLE "/sys/touchscreen/wakeup_gesture_enable"
 
 #define VID_ENC_TIMER_RATE 30000
 #define VID_ENC_IO_IS_BUSY 0
@@ -40,187 +45,134 @@ enum {
     PROFILE_MAX
 };
 
-typedef struct governor_settings {
-    char *above_hispeed_delay;
-    int boost;
-    int boostpulse_duration;
-    int boostpulse_min_interval;
+typedef struct interactive_governor_settings {
     int go_hispeed_load;
-    int go_hispeed_load_off;
     int hispeed_freq;
-    int hispeed_freq_off;
     int io_is_busy;
-    int min_sample_time;
+    int boostpulse_duration;
     char *target_loads;
-    char *target_loads_off;
-    int timer_rate;
-    int timer_rate_off;
-    int timer_slack;
-    // freq.
-    unsigned long scaling_max_freq;
-    unsigned long scaling_min_freq;
-    // hmp
-    int down_threshold;
-    int up_threshold;
-    int task_fork_on_bigcluster;
-    // gpu
-    unsigned long max_freq;
-    unsigned long min_freq;
-    int polling_interval;
-} power_profile;
+    int scaling_min_freq;
+    int scaling_max_freq;
+} power_profile_cpu0;
 
-static power_profile profiles0[PROFILE_MAX] = {
+typedef struct ondemand_governor_settings {
+    int io_is_busy;
+    int sampling_down_factor;
+    int sampling_rate;
+    int up_threshold;
+    int scaling_min_freq;
+    int scaling_max_freq;
+} power_profile_cpu4;
+
+typedef struct other_settings {
+    int hmp_up;
+    int hmp_down;
+    int hmp_prio;
+    unsigned long ddr_max_freq;
+    unsigned long ddr_min_freq;
+    int ddr_polling_interval;
+    unsigned long gpu_max_freq;
+    unsigned long gpu_min_freq;
+    int gpu_polling_interval;
+    int animation_boost;
+    unsigned long animation_boost_freq;
+} power_profile_other;
+
+static power_profile_cpu0 profiles0[PROFILE_MAX] = {
     [PROFILE_POWER_SAVE] = {
-        .above_hispeed_delay = "20000",
-        .boost = 0,
-        .boostpulse_duration = 80000,
-        .boostpulse_min_interval = 500000,
         .go_hispeed_load = 99,
-        .go_hispeed_load_off = 110,
         .hispeed_freq = 1209600,
-        .hispeed_freq_off = 1209600,
         .io_is_busy = 0,
-        .min_sample_time = 80000,
-        .target_loads = "70 604800:80 806400:90 1017600:95",
-        .target_loads_off = "95 1209600:99",
-        .timer_rate = 20000,
-        .timer_rate_off = 50000,
-        .timer_slack = 80000,
-        .scaling_max_freq = 1516800,
+        .boostpulse_duration = 80000,
+        .target_loads = "95",
         .scaling_min_freq = 403200,
-	.down_threshold = 512,
-	.up_threshold = 1024,
-	.task_fork_on_bigcluster = 0,
-	.max_freq = 900000000,
-	.min_freq = 360000000,
-	.polling_interval = 20,
+        .scaling_max_freq = 806400,
     },
     [PROFILE_BALANCED] = {
-        .above_hispeed_delay = "20000",
-        .boost = 0,
-        .boostpulse_duration = 80000,
-        .boostpulse_min_interval = 500000,
-        .go_hispeed_load = 95,
-        .go_hispeed_load_off = 110,
+        .go_hispeed_load = 99,
         .hispeed_freq = 1209600,
-        .hispeed_freq_off = 1209600,
         .io_is_busy = 1,
-        .min_sample_time = 80000,
-        .target_loads = "70 806400:80 1018000:90 1209600:95",
-        .target_loads_off = "95 1305600:99",
-        .timer_rate = 20000,
-        .timer_rate_off = 50000,
-        .timer_slack = 80000,
-        .scaling_max_freq = 1516800,
+        .boostpulse_duration = 80000,
+        .target_loads = "70:604800:75:806400:90:1209600:95",
         .scaling_min_freq = 403200,
-	.down_threshold = 384,
-	.up_threshold = 700,
-	.task_fork_on_bigcluster = 1,
-	.max_freq = 900000000,
-	.min_freq = 360000000,
-	.polling_interval = 40,
+        .scaling_max_freq = 1516800,
     },
     [PROFILE_HIGH_PERFORMANCE] = {
-        .above_hispeed_delay = "20000",
-        .boost = 1,
-        .boostpulse_duration = 80000,
-        .boostpulse_min_interval = 500000,
-        .go_hispeed_load = 90,
-        .go_hispeed_load_off = 110,
-        .hispeed_freq = 1401600,
-        .hispeed_freq_off = 1401600,
+        .go_hispeed_load = 95,
+        .hispeed_freq = 1516800,
         .io_is_busy = 1,
-        .min_sample_time = 80000,
-        .target_loads = "70 1305600:80 1401600:90 1516800:95",
-        .target_loads_off = "95 1516800:99",
-        .timer_rate = 20000,
-        .timer_rate_off = 50000,
-        .timer_slack = 80000,
-        .scaling_max_freq = 1516800,
+        .boostpulse_duration = 160000,
+        .target_loads = "30:604800:40:806400:50:1209600:85",
         .scaling_min_freq = 403200,
-	.down_threshold = 256,
-	.up_threshold = 665,
-	.task_fork_on_bigcluster = 1,
-	.max_freq = 900000000,
-	.min_freq = 360000000,
-	.polling_interval = 50,
+        .scaling_max_freq = 1516800,
     },
 };
 
-static power_profile profiles1[PROFILE_MAX] = {
+static power_profile_cpu4 profiles1[PROFILE_MAX] = {
     [PROFILE_POWER_SAVE] = {
-        .above_hispeed_delay = "20000",
-        .boost = 0,
-        .boostpulse_duration = 80000,
-        .boostpulse_min_interval = 500000,
-        .go_hispeed_load = 99,
-        .go_hispeed_load_off = 110,
-        .hispeed_freq = 1708800,
-        .hispeed_freq_off = 1708800,
         .io_is_busy = 0,
-        .min_sample_time = 80000,
-        .target_loads = "80 1209600:81 1401600:85 1612800:95",
-        .target_loads_off = "95 1708800:99",
-        .timer_rate = 20000,
-        .timer_rate_off = 50000,
-        .timer_slack = 80000,
-        .scaling_max_freq = 1708800,
+        .sampling_down_factor = 1,
+        .sampling_rate = 10000,
+        .up_threshold = 99,
         .scaling_min_freq = 1017600,
-	.down_threshold = 512,
-	.up_threshold = 1024,
-	.task_fork_on_bigcluster = 0,
-	.max_freq = 900000000,
-	.min_freq = 360000000,
-	.polling_interval = 20,
+        .scaling_max_freq = 1209600,
     },
     [PROFILE_BALANCED] = {
-        .above_hispeed_delay = "20000",
-        .boost = 0,
-        .boostpulse_duration = 80000,
-        .boostpulse_min_interval = 500000,
-        .go_hispeed_load = 95,
-        .go_hispeed_load_off = 110,
-        .hispeed_freq = 1804800,
-        .hispeed_freq_off = 1804800,
-        .io_is_busy = 0,
-        .min_sample_time = 80000,
-        .target_loads = "80 1401600:81 1612800:85 1708800:95",
-        .target_loads_off = "95 1804800:99",
-        .timer_rate = 20000,
-        .timer_rate_off = 50000,
-        .timer_slack = 80000,
-        .scaling_max_freq = 2016000,
+        .io_is_busy = 1,
+        .sampling_down_factor = 1,
+        .sampling_rate = 10000,
+        .up_threshold = 99,
         .scaling_min_freq = 1017600,
-	.down_threshold = 384,
-	.up_threshold = 700,
-	.task_fork_on_bigcluster = 1,
-	.max_freq = 900000000,
-	.min_freq = 360000000,
-	.polling_interval = 40,
+        .scaling_max_freq = 2016000,
     },
     [PROFILE_HIGH_PERFORMANCE] = {
-        .above_hispeed_delay = "20000",
-        .boost = 1,
-        .boostpulse_duration = 80000,
-        .boostpulse_min_interval = 500000,
-        .go_hispeed_load = 90,
-        .go_hispeed_load_off = 110,
-        .hispeed_freq = 2016000,
-        .hispeed_freq_off = 2016000,
         .io_is_busy = 1,
-        .min_sample_time = 80000,
-        .target_loads = "80 1708800:81 1804800:85 2016000:95",
-        .target_loads_off = "95 2016000:99",
-        .timer_rate = 20000,
-        .timer_rate_off = 50000,
-        .timer_slack = 80000,
-        .scaling_max_freq = 2016000,
+        .sampling_down_factor = 4,
+        .sampling_rate = 50000,
+        .up_threshold = 85,
         .scaling_min_freq = 1017600,
-	.down_threshold = 256,
-	.up_threshold = 665,
-	.task_fork_on_bigcluster = 1,
-	.max_freq = 900000000,
-	.min_freq = 360000000,
-	.polling_interval = 50,
+        .scaling_max_freq = 2016000,
+    },
+};
+
+static power_profile_other profiles2[PROFILE_MAX] = {
+    [PROFILE_POWER_SAVE] = {
+        .hmp_up = 1008,
+        .hmp_down = 768,
+        .hmp_prio = 140,
+        .ddr_max_freq = 360000000,
+        .ddr_min_freq = 120000000,
+        .ddr_polling_interval = 20,
+        .gpu_max_freq = 360000000,
+        .gpu_min_freq = 288000000,
+        .gpu_polling_interval = 20,
+        .animation_boost = 0,
+        .animation_boost_freq = 480000000,
+    },
+    [PROFILE_BALANCED] = {
+        .hmp_up = 978,
+        .hmp_down = 672,
+        .hmp_prio = 140,
+        .ddr_max_freq = 667000000,
+        .ddr_min_freq = 120000000,
+        .ddr_polling_interval = 40,
+        .gpu_max_freq = 480000000,
+        .gpu_min_freq = 288000000,
+        .gpu_polling_interval = 40,
+        .animation_boost = 1,
+        .animation_boost_freq = 600000000,
+    },
+    [PROFILE_HIGH_PERFORMANCE] = {
+        .hmp_up = 500,
+        .hmp_down = 200,
+        .hmp_prio = 140,
+        .ddr_max_freq = 800000000,
+        .ddr_min_freq = 120000000,
+        .ddr_polling_interval = 50,
+        .gpu_max_freq = 600000000,
+        .gpu_min_freq = 288000000,
+        .gpu_polling_interval = 50,
+        .animation_boost = 1,
+        .animation_boost_freq = 680000000,
     },
 };
