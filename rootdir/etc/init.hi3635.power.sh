1c472f3d9fcd94fa9ab2300097144d46f425fdb3
Andrea Dieni <andrea.dieni@gmail.com>
2017-05-04 22:47:27 +0200

    Ramdisk totally reworked
    Change-Id: Ibcc40404b1298c8ba14ebc765dab5732fb2654ea

--- rootdir/etc/init.hi3635.power.sh
+++ rootdir/etc/init.hi3635.power.sh
@@ -18,16 +18,15 @@ export PATH=/system/bin
 
 for ddrfreq_gov in /sys/class/devfreq/ddrfreq/governor
 do
-	echo "pm_qos" > $ddrfreq_gov
+	echo "mali_ondemand" > $ddrfreq_gov
 done
 
 for ddrfreq_up_threshold_gov in /sys/class/devfreq/ddrfreq_up_threshold/governor
 do
-	echo "pm_qos" > $ddrfreq_up_threshold_gov
+	echo "mali_ondemand" > $ddrfreq_up_threshold_gov
 done
 
 for gpufreq_gov in /sys/class/devfreq/gpufreq/governor
 do
         echo "mali_ondemand" > $gpufreq_gov
 done
-
